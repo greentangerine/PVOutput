b938513948cbf65938ed2af9c1c0b2fa8ab45195
greentangerine <nick@ayle.net>
2020-05-01 11:26:22 +0100

    wireless relay control

--- ts_mqtt.py
+++ ts_mqtt.py
@@ -8,44 +8,102 @@ import sys
 sys.path.insert(0, '/home/pi/ME3000')
 from MyKeys import *
 
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        client.connected_flag = True 
-        print("connected OK Returned code=", rc)
-        client.subscribe(TS1)
-        client.subscribe(TS2)
-    else:
-        print("Bad connection Returned code= ", rc)
-        client.bad_connection_flag = True
-
-mqtt.Client.bad_connection_flag = False
-mqtt.Client.connected_flag = False
-
-client = mqtt.Client(TS_NAME)
-client.on_connect = on_connect
-client.loop_start()
-client.connect(MQTT_HOST)
-
-while not client.connected_flag and not client.bad_connection_flag:
-    time.sleep(1)
-
-if client.bad_connection_flag:
-    client.loop_stop()
-    print("MQTT connection failed ...")
-    quit()
 
 # topic for thermostats on the thermal stores
-TS_1 = 'emon/emonth6/external temperature'
-TS_2 = 'emon/emonth7/external temperature'
+TS_1 = 'thermal_store/ts1'
+TS_2 = 'thermal_store/ts2'
 
 # topics and values for relay control
-RELAY1 = 'immersion/thermal_store'
-RELAY2 = 'immersion/buffer_store'
+RELAY1 = 'emon/immersion/thermal_store'
+RELAY2 = 'emon/immersion/buffer_store'
 R_ON = '1'
 R_OFF = '0'
 
-ret = client.publish(RELAY1, R_ON)
-if ret[0] != 0:
-    print("Publish failed for" + RELAY1)
 
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("connected OK Returned code =", rc)
+
+        ret = client.subscribe([(TS_1, 2),(TS_2, 2),(RELAY1, 2),(RELAY2, 2)])
+        if ret[0] != 0:
+            print("Subscribe failed")
+
+        # ensure both thermostats are initially off ...
+        ret = client.publish(RELAY1, R_OFF)
+        ret = client.publish(RELAY2, R_OFF)
+
+def on_message(client, userdata, message):
+    mesg = message.payload.decode()
+    print(mesg)
+
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("Subscribed: "+ str(mid) + " " + str(granted_qos))
+
+
+def on_publish(client, userdata, mid):
+    sub_id = mid
+    #print("Publish OK");
+
+
+def ts1_callback(client, userdata, message):
+    ts1_temp = message.payload.decode()
+    #print("TS1 temp =", ts1_temp)
+    do_control()
+
+
+def ts2_callback(client, userdata, message):
+    ts2_temp = message.payload.decode()
+    #print("TS2 temp =", ts2_temp)
+    do_control()
+
+
+def relay1_callback(client, userdata, message):
+    r1_stat = message.payload.decode()
+    #print("Relay1 status =", r1_stat)
+
+
+def relay2_callback(client, userdata, message):
+    r2_stat = message.payload.decode()
+    #print("Relay2 status =", r2_stat)
+
+
+def do_control():
+    if ts1_temp >= TS1_MAXTEMP:
+        # always switch off
+        ret = client.publish(RELAY1, R_OFF)
+    
+        if ts2_temp < TS2_MAXTEMP:
+            ret = client.publish(RELAY2, R_ON)
+        else:
+            ret = client.publish(RELAY2, R_OFF)
+    else:
+        # ts1 < threshold
+        # ensure ts2 is off ...
+        ret = client.publish(RELAY2, R_OFF)
+        ret = client.publish(RELAY1, R_ON)
+
+
+# main code ...
+client = mqtt.Client(TS_NAME)
+
+# callbacks
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_subscribe = on_subscribe
+client.on_publish = on_publish
+
+# initial control temperatures
+ts1_temp = 0
+ts2_temp = 0
+r1_stat = None
+r2_stat = None
+
+client.connect(MQTT_HOST)
+client.message_callback_add(TS_1, ts1_callback)
+client.message_callback_add(TS_2, ts2_callback)
+client.message_callback_add(RELAY1, relay1_callback)
+client.message_callback_add(RELAY2, relay2_callback)
+
+client.loop_forever()
 client.disconnect()
